16042c046da7f2bab1e357df37f87fc0e659513d
Marc Beunardeau <mbeunard@dptinfo29.dptinfo.ens-cachan.fr>
2013-03-22 11:41:39 +0100

    globaldata

--- GlobalData.java
+++ GlobalData.java
@@ -7,4 +7,19 @@ public class GlobalData {
 	/** List of all airports */ 
 	private java.util.List<Airport> airports;
 	private Wheather wheather;
+	Collection<FlightID> getWaitingPlanes (id:AirportID) {
+		int i = 0;
+		while(AirportCharacteristics.get(i).id != id){
+			i++ 
+			}
+		return AirportCharacteristics.get(i).getWaitingsPlanes()
+		}
+	AirportStatus getStatus(id:AirportId){	
+		int i = 0;
+		while(AirportCharacteristics.get(i).id != id){
+			i++ 
+			}
+		return AirportCharacteristics.get(i).getStatus()
+		}
 }
+
