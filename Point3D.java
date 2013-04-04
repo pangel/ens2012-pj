2946424d93a97b0b16ede8385a0170961d1cde61
Marc <marc_beunardeau@hotmail.fr>
2013-04-04 22:00:13 +0200

    point

--- Point3D.java
+++ Point3D.java
@@ -2,21 +2,51 @@
  * 
  * x,y,z are the Cartesian coordinates in space, where z stands for the height.
  */
+/**
+ * 
+ * x,y,z are the Cartesian coordinates in space, where z stands for the height.
+ */
 public class Point3D {
 	double x, y, z;
-    public double distance(Point3D a, Point3D b) {
-	return (Math.sqrt ((Math.pow (a.x -b.x) 2) + (Math.pow (a.y -b.y) 2) + (Math.pow (a.z -b.z) 2)))
+    public static double distance(Point3D a, Point3D b) {
+    	double i =2;
+    	return (Math.sqrt((Math.pow((a.x -b.x), i)) + (Math.pow((a.y -b.y), i)) + (Math.pow((a.z -b.z), i))));
 	    }
-    public Point3D moins(Point3D a, Point3D b) {
-	Point3D c = new Point3D;
+    
+    Point3D(double x,double y,double z) {
+    	this.x = x;
+    	this.y = y;
+    	this.z = z;
+    	
+    }
+    
+    public static Point3D moins(Point3D a, Point3D b) {
+	Point3D c = new Point3D((a.x - b.x), (a.y - b.y), (a.z - b.z));
 	c.x = a.x - b.x;
 	c.y = a.y - b.y;
 	c.z = a.z - b.z;
-	return c
-	    }
-    public Point3D plus(Point3D a, Point3D b) {
-	Point3D c = new Point3D;
+	return c;
+	    }	
+    public static Point3D plus(Point3D a, Point3D b) {
+	Point3D c = new Point3D(a.x,a.x,a.x);
 	c.x = a.x + b.x;
 	c.y = a.y + b.y;
 	c.z = a.z + b.z;
-	return c 
+	return c; 
+    	}
+    public static Point3D div (Point3D a, double b)
+    {Point3D c = new Point3D(a.x,a.x,a.x);
+    c.x = a.x/b;
+    c.y = a.y/b;
+    c.z = a.z/b;
+    return c;
+    }
+
+    public static Point3D fois (Point3D a, double b)
+    {Point3D c = new Point3D(a.x,a.x,a.x);
+    c.x = a.x*b;
+    c.y = a.y*b;
+    c.z = a.z*b;
+    return c;
+    }
+}
