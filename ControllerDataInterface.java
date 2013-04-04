addfc794545f06839259d9442421e16bdfafce34
Adrien Husson <pangel.neu@gmail.com>
2013-04-04 23:53:55 +0200

    fixing again

--- ControllerDataInterface.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public interface ControllerDataInterface {
-	public Collection<AirportCharacteristics>AirportCharacteristics();
-	public AirportStatus getStatus(AirportID id);
-	public Collection<FlightID> getWaitingPlanes(AirportID id);
-	public Airport getAirportByID(AirportID id);
-}
--- src/Trajectory.java
+++
@@ -1,6 +0,0 @@
-
-public interface Trajectory {
-
-    
-    public boolean terminated();
-}
